09bc916c533f836f99083f518df2a7966df8e01e
xigma0512 <xig1517@gmail.com>
2025-09-15 21:44:50 +0800

    add baseline model (with most frequency)

---
+++ baseline.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.metrics import accuracy_score
+from data_preprocess import preprocessing
+
+X_train, y_train, X_val, y_val, X_test, y_test = preprocessing()
+
+n_zeros = np.sum(y_train == 0)
+n_ones = np.sum(y_train == 1)
+
+if n_ones > n_zeros:
+    most_frequent = 1
+else:
+    most_frequent = 0
+
+predictions = np.full(y_test.shape, most_frequent)
+accuracy = accuracy_score(y_test, predictions)
+
+print(f'baseline model accuracy score: {accuracy}')
