e26c75a100b42eb3672943709bc588f099da4484
xigma0512 <xig1517@gmail.com>
2025-09-16 17:31:47 +0800

    add histroy price data of `1101.TW` and `AAPL`

--- data/data_crawler.py
+++ data/data_crawler.py
@@ -1,4 +1,4 @@
 import yfinance as yf
 
-df = yf.Ticker("2330.TW").history(period="max")
-df.to_csv('price_2330.csv', sep=',')
+df = yf.Ticker("AAPL").history(period="max")
+df.to_csv('price_AAPL.csv', sep=',')
