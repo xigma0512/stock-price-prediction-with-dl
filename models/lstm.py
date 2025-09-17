396c2d2c7092e6dbe41b56abc10dfe06b1e7ba82
xigma0512 <xig1517@gmail.com>
2025-09-17 11:03:00 +0800

    add basic lstm model

---
+++ models/lstm.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=4, hidden_size=50, num_layers=1, dropout_rate=0.2):
+        super(LSTM, self).__init__()
+
+        self.lstm = nn.LSTM(
+            input_size=input_size, 
+            hidden_size=hidden_size, 
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout_rate if num_layers > 1 else 0
+        )
+
+        self.dropout = nn.Dropout(dropout_rate)
+        self.dense = nn.Linear(in_features=hidden_size, out_features=1)
+        
+    def forward(self, x):
+        lstm_out, _ = self.lstm(x)
+        final_output = lstm_out[:, -1, :]
+        dropout_out = self.dropout(final_output)
+        dense_out = self.dense(dropout_out)
+        output = torch.sigmoid(dense_out)
+        
+        return output
---
+++ run.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import accuracy_score
+
+from utils.preprocess import get_data_loaders
+from utils.plot import plot_graph
+from models.lstm import LSTM
+
+train_loader, val_loader, test_loader = get_data_loaders(n=100, test_size=0.2, val_size=0.1, batch_size=64)
+
+def train(model, optimizer, num_epochs = 100):
+
+    train_losses, val_losses = [], []
+    train_accuracies, val_accuracies = [], []
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        y_true_train, y_pred_train = [], []
+
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+
+            loss = nn.BCELoss()(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * inputs.size(0)
+
+            preds = (outputs > 0.5).int().flatten()
+            y_true_train.extend(labels.flatten().tolist())
+            y_pred_train.extend(preds.tolist())
+
+        avg_train_loss = running_loss / len(train_loader.dataset)
+        train_accuracy = accuracy_score(y_true_train, y_pred_train)
+
+        model.eval()
+        val_loss = 0.0
+        y_true_val, y_pred_val = [], []
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                outputs = model(inputs)
+                loss = nn.BCELoss()(outputs, labels)
+                val_loss += loss.item() * inputs.size(0)
+
+                preds = (outputs > 0.5).int().flatten()
+                y_true_val.extend(labels.flatten().tolist())
+                y_pred_val.extend(preds.tolist())
+
+        avg_val_loss = val_loss / len(val_loader.dataset)
+        val_accuracy = accuracy_score(y_true_val, y_pred_val)
+
+        train_losses.append(avg_train_loss)
+        val_losses.append(avg_val_loss)
+        train_accuracies.append(train_accuracy)
+        val_accuracies.append(val_accuracy)
+
+        print(f"Epoch {epoch+1}/{num_epochs}, Train Acc: {train_accuracy:.4f}, Train Loss: {avg_train_loss:.4f}, Val Acc: {val_accuracy:.4f}, Val Loss: {avg_val_loss:.4f}")
+
+    plot_graph('Loss', train_losses, val_losses)
+    plot_graph('Accuracy', train_accuracies, val_accuracies)
+    return model
+
+
+def eval(model):
+    model.eval()
+    test_loss = 0.0
+    y_true_test, y_pred_test = [], []
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs)
+            loss = nn.BCELoss()(outputs, labels)
+            test_loss += loss.item() * inputs.size(0)
+
+            preds = (outputs > 0.5).int().flatten()
+            y_true_test.extend(labels.flatten().tolist())
+            y_pred_test.extend(preds.tolist())
+
+    avg_test_loss = test_loss / len(test_loader.dataset)
+    test_accuracy = accuracy_score(y_true_test, y_pred_test)
+
+    print(f"✅ Test Accuracy: {test_accuracy:.4f}")
+    print(f"\nTest Loss: {avg_test_loss:.4f}")
+
+if __name__ == "__main__":
+
+    lstm_model = LSTM(
+        input_size=4,
+        hidden_size=50,
+        num_layers=1,
+        dropout_rate=0.2
+    )
+    lstm_optimizer = optim.Adam(lstm_model.parameters(), lr=0.001)
+    trained_lstm_model = train(lstm_model, lstm_optimizer, num_epochs = 200)
+    eval(trained_lstm_model)
