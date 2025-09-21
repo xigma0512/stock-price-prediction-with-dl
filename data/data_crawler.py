eb1836ffaa5493bc721811526dc2637c81832ea3
xigma0512 <xig1517@gmail.com>
2025-09-22 00:18:11 +0800

    合併多支股票數據成一個檔案

--- data/data_crawler.py
+++ data/data_crawler.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import yfinance as yf
 
 stocks = [
@@ -8,6 +9,14 @@ stocks = [
     "META"
 ]
 
+stocks_df = []
 for name in stocks:
     df = yf.Ticker(name).history(period="max", interval="1d", start='2015-01-01')
-    df.to_csv(f'{name}.csv', sep=',')
+    df["Ticker"] = name
+    df = df.ffill()
+    stocks_df.append(df)
+
+all_stocks_df = pd.concat(stocks_df)
+all_stocks_df = all_stocks_df.sort_values(by=['Ticker', 'Date']).reset_index()
+all_stocks_df.to_csv('train_dataset.csv', sep=',')
+print(all_stocks_df.head())
