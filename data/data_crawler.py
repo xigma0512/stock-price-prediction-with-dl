e12bd5d72314a2073fa9ca54f124fc3fa391e19a
xigma0512 <xig1517@gmail.com>
2025-09-22 00:07:09 +0800

    抓多支美股來當訓練資料

--- data/data_crawler.py
+++ data/data_crawler.py
@@ -1,4 +1,13 @@
 import yfinance as yf
 
-df = yf.Ticker("AAPL").history(period="max", interval="1d", start='2010-01-01')
-df.to_csv('price_AAPL.csv', sep=',')
+stocks = [
+    "AAPL",
+    "GOOG",
+    "MSFT",
+    "TSLA",
+    "META"
+]
+
+for name in stocks:
+    df = yf.Ticker(name).history(period="max", interval="1d", start='2015-01-01')
+    df.to_csv(f'{name}.csv', sep=',')
