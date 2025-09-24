6d76cd4b9d875763a6c5912012782fcf0e963eb8
xigma0512 <xig1517@gmail.com>
2025-09-24 17:34:44 +0800

    修改 `get_data_loader` 裡面的命名

--- data/data_loader.py
+++ data/data_loader.py
@@ -5,7 +5,7 @@ def get_data_loader(X, y, batch_size = 64):
     X_tensor = torch.tensor(X, dtype=torch.float32)
     y_tensor = torch.tensor(y, dtype=torch.float32).unsqueeze(1)
 
-    train_dataset = TensorDataset(X_tensor, y_tensor)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    dataset = TensorDataset(X_tensor, y_tensor)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-    return train_loader
+    return data_loader
