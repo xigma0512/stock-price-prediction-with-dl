3179220ac3de4d7b02397889804ae107728fbe7f
xigma0512 <xig1517@gmail.com>
2025-09-24 17:26:42 +0800

    將訓練集和測試集分開

--- data/data_crawler.py
+++ data/data_crawler.py
@@ -2,24 +2,38 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 
-stocks = [
+STOCK_LIST = [
     "AAPL",
     "GOOG",
     "MSFT",
     "TSLA",
-    "META"
+    "META",
+    "AMZN",
+    "NVDA"
 ]
 
-stocks_df = []
-for name in stocks:
+TEST_STOCK = "NFLX"
+TEST_STOCK_PERIOD = "3y"
+
+def get_ticker(name) -> pd.DataFrame:
     df = yf.Ticker(name).history(period="max", interval="1d", start='2015-01-01')
     for col in ['Close', 'High', 'Low', 'Open']:
         df[col] = np.floor(df[col] * 100) / 100
     df["Ticker"] = name
     df = df.ffill()
-    stocks_df.append(df)
+
+    return df
+
+stocks_df = []
+for name in STOCK_LIST:
+    stocks_df.append(get_ticker(name))
 
 all_stocks_df = pd.concat(stocks_df)
 all_stocks_df = all_stocks_df.sort_values(by=['Ticker', 'Date']).reset_index()
-all_stocks_df.to_csv('train_dataset.csv', sep=',')
-print(all_stocks_df.head())
+all_stocks_df.to_csv('data/train_dataset.csv', sep=',')
+
+test_stock_df = get_ticker(name)
+test_stock_df = test_stock_df.sort_values(by=['Ticker', 'Date']).reset_index()
+test_stock_df.to_csv('data/test_dataset.csv', sep=',')
+
+print("done")
